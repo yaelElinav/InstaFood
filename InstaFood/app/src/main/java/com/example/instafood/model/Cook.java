c02df4509d6ac643aa0df710e4e9a8589ec9b0a7
yaelElinav <yaelelinav@gmail.com>
2020-06-16 11:52:54 +0300

    Minor updates.

--- InstaFood/app/src/main/java/com/example/instafood/model/Cook.java
+++ InstaFood/app/src/main/java/com/example/instafood/model/Cook.java
@@ -1,4 +1,7 @@
 package com.example.instafood.model;
 
 public class Cook {
+    int id;
+    String name;
+    // TODO decide how to create the connection between the cook and the dish (one to many relation)
 }
--- InstaFood/app/src/main/java/com/example/instafood/model/Dish.java
+++ InstaFood/app/src/main/java/com/example/instafood/model/Dish.java
@@ -1,6 +1,7 @@
 package com.example.instafood.model;
 
 public class Dish {
+    int id;
     String name;
     Cook maker;
 
