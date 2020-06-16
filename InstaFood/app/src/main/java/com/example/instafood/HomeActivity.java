b79d5e1b787e2e608f064f0f28ea535ada2c8d58
yaelElinav <yaelelinav@gmail.com>
2020-06-16 13:01:10 +0300

    Added Home activity for all the fragments

--- InstaFood/app/src/main/java/com/example/instafood/HomeActivity.java
+++ InstaFood/app/src/main/java/com/example/instafood/HomeActivity.java
@@ -1,14 +1,13 @@
 package com.example.instafood;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
