0352257b525f3ac198c7ebe487d344528f68a118
yaelElinav <yaelelinav@gmail.com>
2020-06-22 22:46:01 +0300

    Oops fixed some bugs

--- InstaFood/app/src/main/java/com/example/instafood/model/AppLocalDb.java
+++ InstaFood/app/src/main/java/com/example/instafood/model/AppLocalDb.java
@@ -18,4 +18,5 @@ public class AppLocalDb{
                     "dbFileName.db")
                     .fallbackToDestructiveMigration()
                     .build();
+
 }
--- InstaFood/app/src/main/java/com/example/instafood/model/Dish.java
+++ InstaFood/app/src/main/java/com/example/instafood/model/Dish.java
@@ -6,23 +6,23 @@ import androidx.room.PrimaryKey;
 
 @Entity
 public class Dish {
-    String id;
     @PrimaryKey
     @NonNull
+    String id;
     String name;
     String imgUrl;
     String makerID;
-    Dish based;
+    String basedOn;
     int likes;
 
     // TODO decide how to create the connection between the dish and it's derivatives (optional)
 
-    public Dish(String id, String name, String imgUrl, String maker, Dish based) {
+    public Dish(String id, String name, String imgUrl, String makerID, String basedOn) {
         this.id = id;
         this.name = name;
         this.imgUrl = imgUrl;
-        this.makerID = maker;
-        this.based = based;
+        this.makerID = makerID;
+        this.basedOn = basedOn;
         this.likes = 0;
     }
 
